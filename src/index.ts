8f2616b3bc5d61e64a356532e88e89f9e8ac3182
EricWithC04 <negativegame117@gmail.com>
2024-08-27 15:52:29 -0300

    feat: initial config for server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express from "express";
+import { Express } from "express";
+
+const app: Express = express()
+const PORT = 3000
+
+app.get("/", (_req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+    console.log("Server running in port 3000")
+})
